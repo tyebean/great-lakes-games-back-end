22940617fa5dbebc70a4e54cf6746cd6883c734a
tyebean <flower.webster@gmail.com>
2022-03-24 17:01:07 -0400

    stub up Game model

---
+++ models/game.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+
+const gameSchema = new mongoose.Schema({
+  apiId: Number,
+  reviews: {type: mongoose.Schema.Types.ObjectId, ref: "Review"},
+},{
+    timestamps: true,
+})
+
+const Game = mongoose.model('Game', gameSchema)
+
+export {Game}
