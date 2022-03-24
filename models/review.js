d3083e26928efba5a674d0031c85f3b49c57faef
tyebean <flower.webster@gmail.com>
2022-03-24 17:15:19 -0400

    add comment schema

--- models/review.js
+++ models/review.js
@@ -1,6 +1,13 @@
 import mongoose from 'mongoose'
 
-// "These [Numbers property] will correspond with third party API"
+const commentSchema = new mongoose.Schema({
+  author: {type: mongoose.Schema.Types.ObjectId, ref: "Profile"},
+  text: String,
+}, {
+  timestamps: true
+})
+
+// "These [Numbers Property] will correspond with third party API"
 const reviewSchema = new mongoose.Schema({
   experience: String,
   //todo: make sure date works correctly when we display it in jsx
@@ -14,4 +21,6 @@ const reviewSchema = new mongoose.Schema({
 
 const Review = mongoose.model('Review', reviewSchema)
 
-export {Review}
+export {
+  Review
+}
