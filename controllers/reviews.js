b80b13a897a6f144e25478184541264f07d268ab
tyebean <flower.webster@gmail.com>
2022-03-25 11:18:17 -0400

    polish review & comment contrl

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -11,44 +11,54 @@ function index(res, req) {
 
 function create(req, res){
   req.body.author = req.user.profile
-  Review.create(req.body)
+  Review.create(req.body) 
   .then(review => {
     review.populate('author')
     .then(populatedReview => {
       res.json(populatedReview)
     })
   })
-  .catch(err => res.json(err))
-}
-
-function deleteReview(req, res){
-  Review.findByIdAndDelete(req.params.id)
-  .then(review => res.json(review))
-  .catch(err => res.json(err))
-}
-
-function show(req, res){
-  Review.findById(req.params.id)
-  .then(review => res.json(review))
-  .catch(err => res.json(err))
-}
-
-function update(req, res){
-  Review.findByIdAndUpdate(req.params.id, req.body, {new: true})
-  .then(review => res.json(review))
-  .catch(err => res.json(err))
-}
-
-function indexComment (res, req) {
-  Comment.find({})
-  .then(comments => res.json(comments))
   .catch(err => {
     console.log(err)
     res.status(500).json(err)
   })
 }
 
-function createComment (req, res){
+function deleteReview(req, res){
+  Review.findByIdAndDelete(req.params.id)
+  .then(review => res.json(review))
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })}
+
+function show(req, res){
+  Review.findById(req.params.id)
+  .then(review => res.json(review))
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })}
+
+function update(req, res){
+  Review.findByIdAndUpdate(req.params.id, req.body, {new: true})
+  .then(review => res.json(review))
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
+}
+
+function indexComment(res, req) {
+  Comment.find({})
+  .then(comment => res.json(comment))
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
+}
+
+function createComment(req, res){
   req.body.author = req.user.profile
   Comment.create(req.body)
   .then(comment => {
@@ -57,19 +67,28 @@ function createComment (req, res){
       res.json(populatedComment)
     })
   })
-  .catch(err => res.json(err))
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
 }
 
 function deleteComment(req, res){
   Comment.findByIdAndDelete(req.params.id)
   .then(review => res.json(review))
-  .catch(err => res.json(err))
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
 }
 
 function showComment (req, res){
   Review.findById(req.params.id)
   .then(review => res.json(review))
-  .catch(err => res.json(err))
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
 }
 
 export {
@@ -78,4 +97,9 @@ export {
   deleteReview as delete,
   show,
   update,
+  indexComment,
+  createComment,
+  showComment,
+  deleteComment
 }
+
