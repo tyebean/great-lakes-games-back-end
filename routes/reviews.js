588f2fa7abc3ca6fffd025aaa642296bdee01a4f
tyebean <flower.webster@gmail.com>
2022-03-27 15:57:50 -0400

    installed express-form-data

--- routes/reviews.js
+++ routes/reviews.js
@@ -4,19 +4,18 @@ import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
 const router = Router()
 
 /*---------- Public Routes ----------*/
-router.get('/reviews', reviewsCtrl.index) 
-router.get('/reviews/:id', reviewsCtrl.show)
+router.get('/', reviewsCtrl.index) 
+router.get('/:id', reviewsCtrl.show)
+router.post('/', reviewsCtrl.create) 
 
-router.get('/reviews/comments', reviewsCtrl.indexComment) 
+router.get('/', reviewsCtrl.indexComment) 
 
 /*---------- Protected Routes ----------*/
 router.use(decodeUserFromToken)
-router.post('/', checkAuth, reviewsCtrl.create)
 router.put('/:id', checkAuth, reviewsCtrl.update)
 router.delete('/:id', checkAuth, reviewsCtrl.delete)
 
-router.post('/comments', checkAuth, reviewsCtrl.createComment)
-router.delete('/comments/:id', checkAuth, reviewsCtrl.deleteComment)
-
+router.post('/', checkAuth, reviewsCtrl.createComment)
+router.delete('/:id', checkAuth, reviewsCtrl.deleteComment)
 
 export { router } 
